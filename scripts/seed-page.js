23a5ac94f7a82ce87e8bdc700f7c73633e29f0ce
kupppo <kupppo@protonmail.com>
2023-03-06 15:59:42 +0000

    animations for downloading

--- scripts/seed-page.js
+++ scripts/seed-page.js
@@ -25,8 +25,14 @@ function downloadFile(data, name) {
 function setupSeedUI() {
   document.addEventListener('seed:ready', (evt) => {
     const btnEl = document.getElementById('download-btn')
-    btnEl.textContent = `Download ${evt.detail.name}`
-    btnEl.disabled = false
+    if (evt.detail.autoDownload) {
+      btnEl.classList.add('downloading')
+      btnEl.textContent = `Downloading...`
+      btnEl.disabled = true
+    } else {
+      btnEl.textContent = `Download ${evt.detail.name}`
+      btnEl.disabled = false
+    }
     btnEl.addEventListener('click', () => {
       downloadFile(evt.detail.data, evt.detail.name)
     })
@@ -37,6 +43,16 @@ function setupSeedUI() {
 
     const numEl = document.getElementById('settings-number')
     numEl.textContent = evt.detail.num
+
+    const seedEl = document.getElementById('seed-container')
+    seedEl.classList.add('loaded')
+  })
+
+  document.addEventListener('seed:download', (evt) => {
+    const btnEl = document.getElementById('download-btn')
+    btnEl.textContent = `Download ${evt.detail.name}`
+    btnEl.disabled = false 
+    btnEl.classList.remove('downloading')
   })
 }
 
@@ -48,10 +64,15 @@ function setupSeedUI() {
     const { num, mode, download: autoDownload } = getSeedOpts()
     const vanillaBytes = await getVanillaBytes()
     const { data, name } = await RandomizeRom(num, mode, {}, { vanillaBytes })
-    const readyEvt = new CustomEvent('seed:ready', { detail: { data, name, num, mode } })
+    const readyEvt = new CustomEvent('seed:ready', { detail: { data, name, num, mode, autoDownload } })
     document.dispatchEvent(readyEvt)
+
     if (autoDownload) {
-      downloadFile(data, name)
+      setTimeout(() => {
+        downloadFile(data, name)
+        const downloadEvt = new CustomEvent('seed:download', { detail: { name } })
+        document.dispatchEvent(downloadEvt)
+      }, 850)
     }
   } catch (e) {
     console.error(e)
