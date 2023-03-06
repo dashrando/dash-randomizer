f98414f03b71d79c5031b80736563256522f798d
kupppo <kupppo@protonmail.com>
2023-03-06 15:18:02 +0000

    initial seed page design with settings

--- scripts/seed-page.js
+++ scripts/seed-page.js
@@ -21,11 +21,34 @@ function downloadFile(data, name) {
  )
 }
 
+function setupSeedUI() {
+  document.addEventListener('seed:ready', (evt) => {
+    const btnEl = document.getElementById('download-btn')
+    btnEl.textContent = `Download ${evt.detail.name}`
+    btnEl.disabled = false
+    btnEl.addEventListener('click', () => {
+      downloadFile(evt.detail.data, evt.detail.name)
+    })
+
+    const modeEl = document.getElementById('settings-mode')
+    const mode = game_modes.find(({ name }) => name === evt.detail.mode)
+    modeEl.textContent = mode.title
+
+    const numEl = document.getElementById('settings-number')
+    numEl.textContent = evt.detail.num
+  })
+}
+
+
+
 (async () => {
   try {
+    setupSeedUI()
     const { num, mode } = getSeedOpts()
     const vanillaBytes = await getVanillaBytes()
     const { data, name } = await RandomizeRom(num, mode, {}, { vanillaBytes })
+    const readyEvt = new CustomEvent('seed:ready', { detail: { data, name, num, mode } })
+    document.dispatchEvent(readyEvt)
     downloadFile(data, name)
   } catch (e) {
     console.error(e)
