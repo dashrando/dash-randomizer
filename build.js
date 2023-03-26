ac24161754cf1d408c310a9eff42fe15435cb473
MassHesteria <brad.hester@gmail.com>
2023-03-26 14:57:00 -0500

    headless working again

--- build.js
+++ build.js
@@ -2,35 +2,24 @@ import * as esbuild from "esbuild";
 import glob from "glob";
 
 async function build() {
-   const paths = await glob("scripts/pages/*.js", { absolute: true });
-   await esbuild.build({
-      entryPoints: paths,
-      bundle: true,
-      minify: true,
-      sourcemap: true,
-      outdir: "public/",
-      entryNames: "[name].bundled",
-   });
-   const cliPath = await glob("scripts/lib/dash-cli.js", { absolute: true });
-   await esbuild.build({
-      entryPoints: cliPath,
-      platform: "node",
-      bundle: true,
-      minify: true,
-      sourcemap: true,
-      conditions: [],
-      outdir: "public/app/",
-      entryNames: "[name].bundled",
-   });
+  const paths = await glob("scripts/pages/*.js", { absolute: true });
+  await esbuild.build({
+    entryPoints: paths,
+    bundle: true,
+    minify: true,
+    sourcemap: true,
+    outdir: "public/",
+    entryNames: "[name].bundled",
+  });
 }
 
 build()
-   .then(() => {
-      console.log("build successful");
-      process.exit(0);
-   })
-   .catch((err) => {
-      console.error("build failed");
-      console.error(err);
-      process.exit(1);
-   });
+  .then(() => {
+    console.log("build successful");
+    process.exit(0);
+  })
+  .catch((err) => {
+    console.error("build failed");
+    console.error(err);
+    process.exit(1);
+  });
