adbaf4bb2972decbdef1c32be79268f74113f87f
kupppo <kupppo@protonmail.com>
2023-03-27 19:51:56 +0100

    build script combined

--- build.js
+++ build.js
@@ -1,7 +1,10 @@
 import * as esbuild from "esbuild";
 import glob from "glob";
+import ncc from "@vercel/ncc";
+import path from "node:path";
+import fs from "node:fs/promises";
 
-async function build() {
+async function buildWeb() {
   const paths = await glob("scripts/pages/*.js", { absolute: true });
   await esbuild.build({
     entryPoints: paths,
@@ -13,6 +16,23 @@ async function build() {
   });
 }
 
+async function buildHeadless() {
+  const entry = path.resolve(process.cwd(), 'headless/index.ts')
+  console.log(entry)
+  const output = await ncc(entry, {
+    minify: true,
+    sourceMapRegister: false,
+    cache: false,
+  })
+  await fs.writeFile(path.resolve(process.cwd(), 'public/app/dash.headless.js'), output.code)
+
+}
+
+const build = async () => Promise.all([
+  buildWeb(),
+  buildHeadless(),
+])
+
 build()
   .then(() => {
     console.log("build successful");
@@ -23,3 +43,4 @@ build()
     console.error(err);
     process.exit(1);
   });
+
