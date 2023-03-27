b56326f3d4d98ebae92dadfd1c3d7c2a8384b437
kupppo <kupppo@protonmail.com>
2023-03-27 15:39:42 +0100

    refactor for got

--- headless/index.ts
+++ headless/index.ts
@@ -1,18 +1,25 @@
 #!/usr/bin/env node
 
 import fs from "fs";
-import http from "http";
 import crypto from "crypto";
 import { program } from "commander";
 import { patchRom } from "../scripts/helpers/patcher";
 import BpsPatch from "../scripts/lib/bps-patch";
 import got from 'got';
 
+export type SeedMode = 
+  "std_mm" |
+  "std_full" |
+  "standard_mm" |
+  "standard_full" |
+  "recall_mm" | 
+  "recall_full"
+
 export type SeedAPIResponse = {
   basePatchUrl: string;
   seedPatch: SeedPatchPart[];
   fileName: string;
-  preset: string;
+  preset: SeedMode;
 }
 
 export type SeedPatchPart = [
@@ -23,133 +30,113 @@ export type SeedPatchPart = [
   }
 ]
 
+export type SeedPatchRequestBody = {
+  preset: string;
+}
+
+export type HeadlessOptions = {
+  baseUrl: string;
+}
+
+const vanillaHash =
+    "12b77c4bc9c1832cee8881244659065ee1d84c70c3d29e6eaf92e6798cc2ca72";
+  const headeredHash =
+    "9a4441809ac9331cdbc6a50fba1a8fbfd08bc490bc8644587ee84a4d6f924fea";
+
+const verify = (rom) => {
+  let hash = crypto.createHash("sha256");
+  hash.update(rom);
+  const signature = hash.digest("hex");
+  if (signature === vanillaHash) {
+    return rom;
+  } else if (signature === headeredHash) {
+    console.warn(
+      "You have entered a headered ROM. The header will now be removed."
+    );
+    const unheaderedContent = rom.slice(512);
+    return verify(unheaderedContent);
+  }
+  throw Error("Invalid vanilla ROM");
+};
+
+function getVanilla(vanillaPath: string) {
+  const fileExists = fs.existsSync(vanillaPath);
+  if (!fileExists) {
+    throw new Error(`Could not find ROM: ${vanillaPath}`);
+  }
+  const contents = fs.readFileSync(vanillaPath)
+  return verify(contents);
+}
+
+const fetchSeedData = async (input: SeedPatchRequestBody, options) => {
+  const url = new URL('/api/seed', options.baseUrl);
+  const res: SeedAPIResponse = await got.post(url.href, {
+    json: input,
+  }).json();
+  return res;
+}
+
+const fetchBasePatch = async (basePatchUrl: string, baseUrl: string) => {
+  const url = new URL(basePatchUrl, baseUrl).href;
+  const { body } = await got.get(url, { responseType: 'buffer' });
+  return body
+}
+
+const createFile = async (vanilla, seedPatch, basePatch, fileName) => {
+  const seedData = seedPatch.map(
+    (h: any[]) => (
+      (
+        [
+          h[0],
+          h[1],
+          new Uint8Array(
+            Object.values(h[2])
+          )
+        ]
+      )
+    )
+  )
+
+  const rom = patchRom(
+    vanilla,
+    new BpsPatch(new Uint8Array(basePatch)),
+    seedData
+  );
+  fs.writeFileSync(fileName, rom);
+  console.log(`Generated: ${fileName}`);
+}
+
 async function main() {
   //-----------------------------------------------------------------
   // Setup command line arguments.
   //-----------------------------------------------------------------
   program
     .argument("<vanillaRom>", "path to vanilla rom")
-    .argument("<preset>", "preset to use");
+    .argument("<preset>", "preset to use")
+    .option('-b --base-url <url>', 'base url for rolling the seed', 'https://dashrando.net/');
 
   program.parse();
 
   const [vanillaPath, preset] = program.args;
+  const options = program.opts();
 
   // Check and verify `vanillaRom`
-  // Check and verify `preset`
+  const vanilla = await getVanilla(vanillaPath);
+
+  // TODO: Check and verify `preset`
+  // verifyPreset(preset);
+
   // Setup options for fetching patch
-  // 
-
-  //-----------------------------------------------------------------
-  // Make sure the vanilla ROM path is correct.
-  //-----------------------------------------------------------------
-
-  if (!fs.existsSync(vanillaPath)) {
-    throw new Error("Could not find ROM: " + vanillaPath);
-  }
-
-  //-----------------------------------------------------------------
-  // Read the vanilla ROM and establish the base URL.
-  //-----------------------------------------------------------------
-
-  const baseUrl = "https://dashrando.net/";
-  const userRom = fs.readFileSync(vanillaPath);
-  const vanillaHash =
-    "12b77c4bc9c1832cee8881244659065ee1d84c70c3d29e6eaf92e6798cc2ca72";
-  const headeredHash =
-    "9a4441809ac9331cdbc6a50fba1a8fbfd08bc490bc8644587ee84a4d6f924fea";
-
-  //-----------------------------------------------------------------
-  // Verify the vanilla ROM checksum.
-  //-----------------------------------------------------------------
-  const verifyAndSetRom = (rom) => {
-    let hash = crypto.createHash("sha256");
-    hash.update(rom);
-    const signature = hash.digest("hex");
-    if (signature === vanillaHash) {
-      return rom;
-    } else if (signature === headeredHash) {
-      console.warn(
-        "You have entered a headered ROM. The header will now be removed."
-      );
-      const unheaderedContent = rom.slice(512);
-      return verifyAndSetRom(unheaderedContent);
-    }
-    throw Error("Invalid vanilla ROM");
-  };
-
-  const vanillaRom = verifyAndSetRom(userRom);
-
-  const postData = JSON.stringify({
-    preset: preset,
-  });
-  console.log(postData, typeof got);
-
+  const seedData = await fetchSeedData({ preset }, options);
+  const basePatch = await fetchBasePatch(seedData.basePatchUrl, options.baseUrl);
+  await createFile(vanilla, seedData.seedPatch, basePatch, seedData.fileName);
   
   return 0;
 }
 
-// function fetchSettings(postData) {
-//   const options = {
-//     hostname: "localhost",
-//     port: 3000,
-//     path: "/api/seed",
-//     method: "POST",
-//     headers: {
-//       "Content-Type": "application/json",
-//       "Content-Length": Buffer.byteLength(postData),
-//     },
-//   };
-
-//   return new Promise((resolve, reject) => {
-  
-//     const req = http.request(options, (res) => {
-//       let data = "";
-//       res.setEncoding("utf8");
-//       res.on("data", (chunk) => {
-//         data += chunk;
-//       });
-//       res.on("end", () => {
-//         const seedData = JSON.parse(data);
-//         const { basePatchUrl, seedPatch, fileName } = seedData;
-
-//         if (!basePatchUrl || !seedPatch || !fileName) {
-//           return reject(`Failed to generate preset: ${preset}`);
-//         }
-
-//         const patch = seedPatch.map((h: any[]) => {
-//           return resolve([h[0], h[1], new Uint8Array(Object.values(h[2]))]);
-//         });
-
-//         fetch(baseUrl + basePatchUrl)
-//           .then((res) => res.arrayBuffer())
-//           .then((buffer) => {
-//             const rom = patchRom(
-//               vanillaRom,
-//               new BpsPatch(new Uint8Array(buffer)),
-//               patch
-//             );
-//             fs.writeFileSync(fileName, rom);
-//             console.log("Generated: " + fileName);
-//           });
-//       });
-//     });
-
-//     req.on("error", (e) => {
-//       console.error(`problem with request: ${e.message}`);
-//     });
-
-//     // // Write data to request body
-//     req.write(postData);
-//     req.end();
-//   });
-// }
-
 main()
   .then((code: number) => {
-    if (code != 0) {
-      process.exit(code);
-    }
+    process.exit(code);
   })
   .catch((e) => {
     console.error(e.message);
