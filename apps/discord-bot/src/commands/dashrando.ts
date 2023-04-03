66d00cfc535785c87a33b2967cb8245f70b5d46c
kupppo <kupppo@protonmail.com>
2023-04-03 16:04:59 +0100

    add repeat option for prefix command

--- apps/discord-bot/src/commands/dashrando.ts
+++ apps/discord-bot/src/commands/dashrando.ts
@@ -1,4 +1,15 @@
 import { SlashCommandBuilder, ChatInputCommandInteraction, Message } from 'discord.js'
+import { program } from 'commander'
+
+export type SeedOptions = {
+  preset: string
+  number?: number
+}
+
+export type BotInputOptions = {
+  preset: string
+  repeat: number
+}
 
 const MODES = [
   { name: 'Standard Major/Minor', value: 'sm' },
@@ -7,28 +18,38 @@ const MODES = [
   { name: 'Recall Full', value: 'rf' },
 ]
 
-const generateUrl = ({ mode = 'recall_mm' }: { mode: string }) => {
+const generateUrl = ({ preset = 'rf' }: { preset: string }) => {
   const seedNum = Math.floor(Math.random() * 999999) + 1
   const url = new URL('https://dashrando.net/seed')
-  url.searchParams.append('mode', mode)
+  url.searchParams.append('mode', preset)
   url.searchParams.append('seed', seedNum.toString())
   return url.toString()
 }
 
-function parseCommandInput(input: string) {
+const getModeFromInput = (input: string) => {
   const regex = /mode=(\w+)/;
   const matches = regex.exec(input);
-  const hasMatch = matches !== null && matches.length >= 2
+  const hasMatch = matches !== null && matches.length >= 2;
   if (!hasMatch) {
-      return null;
+    return null;
   }
-  const modeInput = matches[1]
-  const validMode = MODES.find((mode) => mode.value === modeInput)
+  return matches[1];
+}
+
+function parseCommandInput(input: string) {
+  const userInput = input.replace('!dashrando ', '').trim()
+  program.requiredOption('-p --preset <mode>', 'logic mode to use', 'rm');
+  program.option('-r --repeat <number>', 'number of times to repeat the seed', '1');
+  program.parse(userInput.split(' '), { from: 'user' });
+  const opts = program.opts()
+
+  const validMode = MODES.find((mode) => mode.value === opts.preset)
   if (!validMode) {
     const validModes = MODES.map((mode) => `\`${mode.value}\``)
-    throw new Error(`Invalid mode: ${modeInput}. Please use one of the following: ${validModes.join(', ')}`)
+    throw new Error(`Invalid mode: ${opts.preset}. Please use one of the following: ${validModes.join(', ')}`)
   }
-  return { mode: modeInput };
+  opts.repeat = parseInt(opts.repeat) || 1
+  return opts;
 }
 
 export const slashCommand = {
@@ -37,23 +58,26 @@ export const slashCommand = {
 		.setDescription('Roll a DASH seed')
 		.addStringOption((option) => (
 			option
-        .setName('mode')
+        .setName('preset')
         .setDescription('The logic mode used in rolling the seed')
         .addChoices(...MODES)
 		)),
   async execute(interaction: ChatInputCommandInteraction) {
-    const mode = interaction.options.getString('mode') ?? 'recall_mm'
-    const seedUrl = generateUrl({ mode })
+    const preset = interaction.options.getString('preset') ?? 'recall_mm'
+    const seedUrl = generateUrl({ preset })
     await interaction.reply(seedUrl)
   },
 }
 
 export const prefixCommand = (message: Message) => {
   try {
-    const command = parseCommandInput(message.content)
-    if (command) {
-      const seedUrl = generateUrl(command)
-      message.reply(seedUrl)
+    const options = parseCommandInput(message.content) as BotInputOptions
+    if (options.preset) {
+      const urls = []
+      for (let i = 0; i < options.repeat; i++) {
+        urls.push(generateUrl(options))
+      }
+      message.reply(urls.join('\n'))
     }
   } catch (err) {
     const error = err as Error
