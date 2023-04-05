c878e94537bfe5457182b2ef6238afbf376f84f3
kupppo <kupppo@protonmail.com>
2023-04-05 11:12:32 +0100

    aside has a width

--- apps/web-next/app/logic/[logic]/page.tsx
+++ apps/web-next/app/logic/[logic]/page.tsx
@@ -17,27 +17,31 @@ export default async function LogicPage({ params }: { params: LogicParams }) {
       <div>
         {checks.map((check: any) => (
           <div key={check.key} className={styles.check}>
-            <Type el="h2" weight="bold" family="mono" size="large" className={styles.title}>
-              <span id={check.key} className={styles.anchor_spacer} />
-              <a className={styles.header_link} href={`/logic/${params.logic}#${check.key}`}>
-                {check.key}
-                <span className={styles.link_icon}>
-                  <LinkIcon size={14} />
-                </span>
-              </a>
-            </Type>
-            {check.description}
-            <Spacer y={4} />
-            <Code>{check.fn}</Code>
-            <Spacer y={2} />
-            <Caption>
-              <a href={check.url} target="_blank" className={styles.external_link}>
-                View on Github
-                <span className={styles.external_icon}>
-                  <ExternalLink size={12} />
-                </span>
-              </a>
-            </Caption>
+            <article className={styles.check_content}>
+              <Type el="h2" weight="bold" family="mono" size="large" className={styles.title}>
+                <span id={check.key} className={styles.anchor_spacer} />
+                <a className={styles.header_link} href={`/logic/${params.logic}#${check.key}`}>
+                  {check.key}
+                  <span className={styles.link_icon}>
+                    <LinkIcon size={14} />
+                  </span>
+                </a>
+              </Type>
+              {check.description}
+            </article>
+            <aside className={styles.sidebar}>
+              <Spacer y={4} />
+              <Code>{check.fn}</Code>
+              <Spacer y={2} />
+              <Caption>
+                <a href={check.url} target="_blank" className={styles.external_link}>
+                  View on Github
+                  <span className={styles.external_icon}>
+                    <ExternalLink size={12} />
+                  </span>
+                </a>
+              </Caption>
+            </aside>
           </div>
         ))}
       </div>
