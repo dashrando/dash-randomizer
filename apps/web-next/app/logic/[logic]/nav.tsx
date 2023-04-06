e35d7322eaf30f6e0f610680628ea087628a6280
kupppo <kupppo@protonmail.com>
2023-04-06 15:29:49 +0100

    update text for some checks in recall

--- apps/web-next/app/logic/[logic]/nav.tsx
+++ apps/web-next/app/logic/[logic]/nav.tsx
@@ -1,6 +1,5 @@
 import Modes from 'core/modes'
 import Type from '@/app/components/typography'
-import Link from 'next/link'
 import styles from './nav.module.css'
 
 const Nav = async ({ modeKey }: { modeKey: string }) => {
@@ -11,12 +10,20 @@ const Nav = async ({ modeKey }: { modeKey: string }) => {
     <aside className={styles.wrapper}>
       <nav className={styles.nav}>
         <Type size="small" weight="bold" className={styles.title}>Logical Checks</Type>
-        <ul>
+        <ul className={styles.logical_list}>
           {checks.map((check: any) => (
             <li key={check}>
               <a href={`#${check}`}>{check}</a>
             </li>
           ))}
+        </ul>
+        <Type size="small" weight="bold" className={styles.title}>Item Locations</Type>
+        <ul>
+          {/* {checks.map((check: any) => (
+            <li key={check}>
+              <a href={`#${check}`}>{check}</a>
+            </li>
+          ))} */}
         </ul>
       </nav>
     </aside>
--- apps/web-next/app/logic/[logic]/recall.tsx
+++ apps/web-next/app/logic/[logic]/recall.tsx
@@ -1,5 +1,11 @@
 import { Body } from '@/app/components/text'
 
+const ExternalLink = ({ href, children, ...props }: any) => (
+  <a href={href} target="_blank" rel="noopener noreferrer" {...props}>
+    {children}
+  </a>
+)
+
 const data = new Map()
 data.set('canHellRun',
   (
@@ -7,6 +13,33 @@ data.set('canHellRun',
       <Body>
         A hell run refers to traversing a set of rooms in a heated environment (ie many rooms in Norfair) without heat protection or limited heat protection. This check will determine whether you are able to hell run through following routes:
       </Body>
+      <ul>
+        <li>
+          <Body>
+            <ExternalLink href="https://wiki.supermetroid.run/Cathedral_Entrance" target="_blank">Cathedral</ExternalLink> to <ExternalLink href="https://wiki.supermetroid.run/Bubble_Mountain" target="_blank">Bubble Mountain</ExternalLink>
+          </Body>
+        </li>
+        <li>
+          <Body>
+            <ExternalLink href="https://wiki.supermetroid.run/Kronic_Boost_Room" target="_blank">Kronic Boost</ExternalLink> to <ExternalLink href="https://wiki.supermetroid.run/Bubble_Mountain" target="_blank">Bubble Mountain</ExternalLink>
+          </Body>
+        </li>
+      </ul>
+    </>
+  )
+)
+data.set('canAccessRedBrinstar',
+  (
+    <>
+      <Body>This check will determine whether you are able to venture into Red Brinstar. This check does not assume which direction you enter Red Brinstar from, whether from <ExternalLink href="https://wiki.supermetroid.run/Red_Tower">Red Tower</ExternalLink> or the <ExternalLink href="https://wiki.supermetroid.run/Red_Tower_Elevator_Room">Elevator Room</ExternalLink>.</Body>
+    </>
+  )
+)
+
+data.set('canAccessHeatedNorfair',
+  (
+    <>
+      <Body>This check will determine whether you are able to enter the heated rooms in Norfair.</Body>
     </>
   )
 )
