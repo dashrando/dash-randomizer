7f9a9e41e973cf7f2bfd61cc4779105491d96df9
kupppo <kupppo@protonmail.com>
2023-04-26 07:33:51 +0100

    include spazer for logical access in recall

--- scripts/lib/modes/modeRecall.js
+++ scripts/lib/modes/modeRecall.js
@@ -158,7 +158,7 @@ class ModeRecall {
             canAccessRedBrinstar(load) &&
             load.canUsePowerBombs &&
             (
-               (load.hasGravity && (load.hasIce || load.hasSpeed)) ||
+               (load.hasGravity && (load.hasIce || load.hasSpeed || load.hasSpazer)) ||
                (canDoSuitlessMaridia(load) && (load.hasIce || load.hasSpazer))
             )
          )
