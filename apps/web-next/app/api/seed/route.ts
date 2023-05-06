4f66b69283255d9d37f2545f90a37aebdea4bab7
kupppo <kupppo@protonmail.com>
2023-05-07 00:43:41 +0100

    remove extra bracket in api seed call

--- apps/web-next/app/api/seed/route.ts
+++ apps/web-next/app/api/seed/route.ts
@@ -1,7 +1,4 @@
-// export async function GET(request: Request) {
-//   return new Response('Hello, Next.js!')
-// }
-
+import type { NextRequest } from 'next/server';
 import { generateFromPreset } from "core";
 
 export const config = {
@@ -24,36 +21,34 @@ const resJSON = (data: any, status = 200, headers = {}) => (
   )
 )
 
-export async function POST(req: Request) {
-  {
-    try {
-      const body = await req.json();
-      if (!body.preset) {
-        throw new Error('Missing preset');
-      }
-      const { preset } = body
-      const isValidPreset = validPresets.includes(preset)
-      if (!isValidPreset) {
-        const msg = `Invalid preset. Valid presets are: ${validPresets.slice(0, -1).join(', ')} or ${validPresets.slice(-1)}`
-        const err = new Error(msg) as HTTPError;
-        err.status = 422;
-        throw err;
-      }
-      const [basePatchUrl, seedPatch, fileName] = await generateFromPreset(preset)
-      return resJSON({
-        basePatchUrl,
-        seedPatch,
-        fileName,
-        preset,
-      }, 200, {
-        'Cache-Control': 's-maxage=86400',
-        'Content-Type': 'application/json',
-      })
-    } catch (err: unknown) {
-      console.error(err)
-      const error = err as HTTPError;
-      const status = error.status || 500;
-      return resJSON({ error: error.message }, status);
+export async function POST(req: NextRequest) {
+  try {
+    const body = await req.json();
+    if (!body.preset) {
+      throw new Error('Missing preset');
     }
+    const { preset } = body
+    const isValidPreset = validPresets.includes(preset)
+    if (!isValidPreset) {
+      const msg = `Invalid preset. Valid presets are: ${validPresets.slice(0, -1).join(', ')} or ${validPresets.slice(-1)}`
+      const err = new Error(msg) as HTTPError;
+      err.status = 422;
+      throw err;
+    }
+    const [basePatchUrl, seedPatch, fileName] = await generateFromPreset(preset)
+    return resJSON({
+      basePatchUrl,
+      seedPatch,
+      fileName,
+      preset,
+    }, 200, {
+      'Cache-Control': 's-maxage=86400',
+      'Content-Type': 'application/json',
+    })
+  } catch (err: unknown) {
+    console.error(err)
+    const error = err as HTTPError;
+    const status = error.status || 500;
+    return resJSON({ error: error.message }, status);
   }
 }
