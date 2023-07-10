5f0ce007947511d77b34e76fca6bcb0d6138bdb6
kupppo <kupppo@protonmail.com>
2023-07-10 14:23:24 +0100

    mocking up mobile header

--- apps/web/app/components/wrapper.tsx
+++ apps/web/app/components/wrapper.tsx
@@ -4,11 +4,11 @@ import styles from './wrapper.module.css'
 
 export const Wrapper = (props: PropsWithChildren) => {
   return (
-    <div>
+    <>
       <NewHeader />
       <main className={styles.container}>
         {props.children}
       </main>
-    </div>
+    </>
   )
 }
