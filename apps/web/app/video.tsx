02d29c46c84fcd3414aed9bf67b4ab04f2339973
kupppo <kupppo@protonmail.com>
2023-07-20 12:59:22 +0100

    onerror show fallback

--- apps/web/app/video.tsx
+++ apps/web/app/video.tsx
@@ -9,12 +9,13 @@ import { cn } from '@/lib/utils'
 const HomeVideo = () => {
   const mounted = useMounted()
   const [ready, setReady] = useState(false)
+  const [fallback, setFallback] = useState(false)
   if (!mounted) {
     return null
   }
 
   return (
-    <div className={cn(styles.container, ready && styles.ready)}>
+    <div className={cn(styles.container, ready && styles.ready, fallback && styles.show_fallback)}>
       <ReactPlayer
         url="/recall-bg.mp4"
         playing
@@ -27,6 +28,9 @@ const HomeVideo = () => {
         height="100%"
         onReady={() => {
           setReady(true)
+        }}
+        onError={() => {
+          setFallback(true)
         }}
       />
       <div className={styles.fallback}>
