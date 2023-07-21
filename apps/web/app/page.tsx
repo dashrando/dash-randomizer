fd26208266f6f44bf55fde74baf00ee326da1f17
kupppo <kupppo@protonmail.com>
2023-07-21 01:56:06 +0100

    homepage buttons

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -3,6 +3,7 @@ import Link from 'next/link'
 import { Wrapper } from './components/wrapper'
 import styles from './home.module.css'
 import Video from './video'
+import { ButtonLink } from './components/button'
 
 export const metadata: Metadata = {
   title: 'DASH',
@@ -15,7 +16,20 @@ const Homepage: NextPage = () => {
     <>
       <Video />
       <Wrapper borderless>
-        <h1>Homepage</h1>
+        <div className={styles.container}>
+          <div className={styles.hero}>
+            <h1>DASH: Recall</h1>
+            <h2>A reimagining and rebalancing logic</h2>
+            <div className={styles.actions}>
+              <ButtonLink href="/generate" variant="primary" className={styles.primaryCTA}>
+                Generate a Seed
+              </ButtonLink>
+              <ButtonLink href="/info/recall" variant="secondary" className={styles.secondaryCTA}>
+                Learn more
+              </ButtonLink>
+            </div>
+          </div>
+        </div>
       </Wrapper>
     </>
    )
