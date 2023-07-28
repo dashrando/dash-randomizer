4bc828c6bfc9a88fa87b324a2067e011a3df9000
kupppo <kupppo@protonmail.com>
2023-07-28 12:38:50 +0100

    determine if vanilla rom

--- apps/web/app/components/file-drop.tsx
+++ apps/web/app/components/file-drop.tsx
@@ -2,47 +2,87 @@
 
 import { useDropzone } from 'react-dropzone'
 import { useCallback, useEffect, useState } from 'react'
-import { bytesToParams, paramsToString } from 'core'
+import { bytesToParams, paramsToString, vanilla as vanillaData } from 'core'
 import styles from './file-drop.module.css'
 import { useRouter } from 'next/navigation'
+import { useVanilla } from '../generate/vanilla'
 
-const getParamsFromFile = async (file: any) => {
+const getParamsFromFile = (bytes: Uint8Array) => {
+  try {
+    const offset = 0x2f8b00
+    const paramBytes = bytes.subarray(offset, offset + 6)
+    const byteParams = bytesToParams(paramBytes)
+    const seedKey = paramsToString(
+      byteParams.seed,
+      byteParams.mapLayout,
+      byteParams.itemPoolParams,
+      byteParams.settings,
+      byteParams.options
+    )
+    return seedKey
+  } catch (e) {
+    const err = e as Error;
+    console.error(err.message)
+    // TODO: Present a friendly error message to the user instead of an alert.
+    alert(err.message)
+    return null
+  }
+}
+
+async function checkVanilla(value: Uint8Array): Promise<boolean> {
+  try {
+    await verifyVanilla(value)
+    return true
+  } catch (_) {
+    return false
+  }
+}
+
+async function verifyVanilla(value: any): Promise<any> {
+  const { getSignature, isVerified, isHeadered } = vanillaData
+  const signature = await getSignature(value)
+  if (isVerified(signature)) {
+    return new Uint8Array(value)
+  }
+
+  if (isHeadered(signature)) {
+    console.warn(
+      "You have entered a headered ROM. The header will now be removed."
+    )
+    const unheaderedContent = value.slice(512)
+    return verifyVanilla(unheaderedContent)
+  }
+
+  throw Error("Vanilla Rom does not match checksum.")
+}
+
+const getFileContents = async (file: File): Promise<Uint8Array> => {
   return new Promise((resolve, reject) => {
-    let reader = new FileReader();
-
-  reader.onload = async function () {
-    try {
-      const offset = 0x2f8b00
-      const bytes = new Uint8Array(reader.result as ArrayBuffer)
-      const paramBytes = bytes.subarray(offset, offset + 6)
-      const byteParams = bytesToParams(paramBytes)
-      const seedKey = paramsToString(
-        byteParams.seed,
-        byteParams.mapLayout,
-        byteParams.itemPoolParams,
-        byteParams.settings,
-        byteParams.options
-      )
-      resolve(seedKey)
-    } catch (e) {
-      const err = e as Error;
-      console.error(err.message);
-      // TODO: Present a friendly error message to the user instead of an alert.
-      alert(err.message);
-      reject(err)
+    let reader = new FileReader()
+    reader.onload = async function () {
+      try {
+        const bytes = new Uint8Array(reader.result as ArrayBuffer)
+        resolve(bytes)
+      } catch (e) {
+        const err = e as Error
+        console.error(err.message)
+        // TODO: Present a friendly error message to the user instead of an alert.
+        alert(err.message)
+        reject(err)
+      }
     }
-  };
 
-  reader.onerror = function () {
-    alert("Failed to load file.");
-  };
+    reader.onerror = function () {
+      alert("Failed to load file.")
+    }
 
-  reader.readAsArrayBuffer(file);
+    reader.readAsArrayBuffer(file)
   })
 }
 
 const FileDrop = (props: React.PropsWithChildren) => {
   const [active, setActive] = useState(false)
+  const { data: vanilla, set: setVanilla } = useVanilla()
   const router = useRouter()
 
   const onDragEnter = useCallback((evt: Event) => {
@@ -60,13 +100,24 @@ const FileDrop = (props: React.PropsWithChildren) => {
   const onDrop = useCallback(async (acceptedFiles: any) => {
     setActive(false)
     const file = acceptedFiles[0]
-    // if vanilla, set vanilla if not set
-    // if seed, go to seed page
-    const seedKey = await getParamsFromFile(file)
-    if (seedKey) {
-      router.push(`/seed/${seedKey}`)
+    const data = await getFileContents(file)
+    
+    const isVanilla = await checkVanilla(data)
+    if (isVanilla) {
+      if (vanilla) {
+        return null
+      }
+      await setVanilla(data)
+      return null
     }
-  }, [router, setActive])
+
+    // TODO: Test if DASH seed
+    // if seed, go to seed page
+    const seedKey = getParamsFromFile(data)
+    if (seedKey) {
+      router.push(`/seed/${seedKey}?download=false`)
+    }
+  }, [router, setActive, vanilla, setVanilla])
 
   const { getRootProps, getInputProps, isDragActive } = useDropzone({
     noClick: true,
