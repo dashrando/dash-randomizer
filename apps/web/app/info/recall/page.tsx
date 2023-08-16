4bf6b4fd7c8404121abd0a8b80be1294be4a15b4
kupppo <kupppo@protonmail.com>
2023-08-16 12:56:39 +0100

    placeholder info page

---
+++ apps/web/app/info/recall/page.tsx
@@ -0,0 +1,15 @@
+import { Wrapper } from '@/app/components/wrapper'
+import styles from '../info.module.css'
+
+export default function RecallInfoPage() {
+  return (
+    <Wrapper>
+      <div style={{ maxWidth: '620px', margin: '0 auto' }}>
+        <article className={styles.article}>
+          <h1 className={styles.headline}>DASH: Recall</h1>
+          <p><em>Coming Soon</em></p>
+        </article>
+      </div>
+    </Wrapper>
+  )
+}
