4523898132d64e4d533ed0bc95226ed5ca2d20b6
kupppo <kupppo@protonmail.com>
2023-08-21 00:08:05 +0100

    show all options on seed page

--- apps/web/app/seed/[seed]/seed.tsx
+++ apps/web/app/seed/[seed]/seed.tsx
@@ -5,8 +5,13 @@ import { saveAs } from 'file-saver'
 import useMounted from '@/app/hooks/useMounted'
 import { useVanilla } from '@/app/generate/vanilla'
 import styles from './seed.module.css'
-import { RandomizeRom, fetchSignature, findPreset } from 'core'
-import { MajorDistributionMode } from 'core/params'
+import { RandomizeRom, fetchSignature, findPreset, Item } from 'core'
+import {
+  BeamMode,
+  MajorDistributionMode,
+  MinorDistributionMode,
+  GravityHeatReduction
+} from 'core/params'
 import { cn } from '@/lib/utils'
 import Button from '@/app/components/button'
 
@@ -30,32 +35,85 @@ const getItemSplit = (value: number) => {
   }
 }
 
+const getMinorItemDistribution = (value: number) => {
+  switch (value) {
+    case MinorDistributionMode.Dash:
+      return 'DASH - 2:1:1'
+    case MinorDistributionMode.Standard:
+      return 'Standard - 3:2:1'
+  }
+}
+
+const getMapLayout = (value: number) => {
+  return value === 1 ? 'Standard Vanilla' : 'DASH Recall'
+}
+
 const getBossMode = (value: number) => {
   return (value === 2) ? 'Randomized' : 'Standard'
 }
 
 const getAreaMode = (value: boolean) => value ? 'Randomized' : 'Standard'
 
+const getBeamMode = (value: number) => {
+  switch (value) {
+    case BeamMode.Vanilla:
+      return 'Vanilla'
+    case BeamMode.DashClassic:
+      return 'Classic'
+    case BeamMode.DashRecall:
+      return 'Recall'
+    case BeamMode.New:
+      return 'New'
+  }
+}
+
+const getGravityMode = (value: number) => {
+  return value === GravityHeatReduction.On ? 'On' : 'Off'
+}
+
+const displayOnOff = (value: boolean) => value ? 'On' : 'Off'
+
+const getExtraItems = (extraItems: number[]) => {
+  const doubleJump = displayOnOff(extraItems.includes(Item.DoubleJump))
+  const heatShield = displayOnOff(extraItems.includes(Item.HeatShield))
+  const pressureValve = displayOnOff(extraItems.includes(Item.PressureValve))
+  return { doubleJump, heatShield, pressureValve }
+}
+
 const parseSettings = (parameters: any) => {
-  const { bossMode, randomizeAreas, itemPoolParams } = parameters
+  const { itemPoolParams } = parameters
+  const extraItems = getExtraItems(itemPoolParams.majorDistribution.extraItems)
   const randomizeParams = [
     { label: 'Item Split', value: getItemSplit(itemPoolParams.majorDistribution.mode) },
     { label: 'Boss', value: getBossMode(parameters.settings.bossMode) },
     { label: 'Area', value: getAreaMode(parameters.settings.randomizeAreas) }
   ]
-  return { randomizeParams }
+  const settingsParams = [
+    { label: 'Minor Item Distribution', value: getMinorItemDistribution(itemPoolParams.minorDistribution.mode)},
+    { label: 'Map Layout', value: getMapLayout(parameters.mapLayout) },
+    { label: 'Beam Mode', value: getBeamMode(parameters.settings.beamMode), },
+    { label: 'Gravity Heat Reduction', value: getGravityMode(parameters.settings.gravityHeatReduction), },
+    { label: 'Double Jump', value: extraItems.doubleJump, },
+    { label: 'Heat Shield', value: extraItems.heatShield, },
+    { label: 'Pressure Valve', value: extraItems.pressureValve, },
+  ]
+  const optionsParams = [
+    { label: 'Seed Number', value: parameters.seed },
+    { label: 'Disable Fanfare', value: displayOnOff(parameters.options.DisableFanfare), }
+  ]
+  return { randomizeParams, settingsParams, optionsParams }
 }
 
 const Parameters = ({ title, items }: { title: string, items: any[] }) => {
   return (
     <div>
-      <h4>{title}</h4>
+      <h4 className={styles.heading}>{title}</h4>
       {items.length > 0 && (
-        <ul>
+        <ul className={styles.list}>
           {items.map((item, index) => (
-            <li key={index}>
+            <li key={index} className={styles.item}>
               <div className={styles.label}>{item.label}</div>
-              <div>{item.value}</div>
+              <div className={styles.value}>{item.value}</div>
             </li>
           ))}
         </ul>
@@ -100,7 +158,7 @@ export default function Seed({ parameters }: any) {
   console.log(parameters)
   const parsedParams = parseSettings(parameters)
 
-  console.log(parsedParams.randomizeParams)
+  console.log(parsedParams.settingsParams)
 
   return (
     <div>
@@ -117,8 +175,8 @@ export default function Seed({ parameters }: any) {
         ) : <Button variant="secondary">Upload Vanilla</Button>}
       </div>
       <Parameters title="Randomization" items={parsedParams.randomizeParams} />
-      <Parameters title="Settings" items={[]} />
-      <Parameters title="Options" items={[]} />
+      <Parameters title="Settings" items={parsedParams.settingsParams} />
+      <Parameters title="Options" items={parsedParams.optionsParams} />
     </div>
   )
 }
