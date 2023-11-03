963b43652b7a13a0d8d90598da9e4053a426c532
kupppo <kupppo@protonmail.com>
2023-11-03 11:44:00 +0000

    generate QR code for seed

--- apps/web/app/seed/[seed]/qr/route.ts
+++ apps/web/app/seed/[seed]/qr/route.ts
@@ -1,7 +1,17 @@
 import type { NextRequest } from 'next/server'
+import QRCode from 'qrcode'
 
-export async function GET(_req: NextRequest) {
-  return new Response('OK', {
+export async function GET(req: NextRequest, { params }: { params: { seed: string }}) {
+  const seedURL = new URL(req.nextUrl.href.replace('/qr', ''))
+  const url = seedURL.href
+  const svg = await QRCode.toString(url, { type: 'svg', margin: 0 })
+  const headers = new Headers()
+  headers.set('Content-Type', 'image/svg+xml')
+  // This will cause the qr code to download
+  // headers.set('Content-Disposition', `attachment; filename="${params.seed}.svg"`)
+  headers.set('X-Cache-Control', 's-maxage=86400')
+  return new Response(svg, {
     status: 200,
+    headers,
   });
 }
