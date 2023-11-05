9a16f4a2175ba94ca2d8b6c11e190ced0dc4d6e2
MassHesteria <brad.hester@gmail.com>
2023-11-05 09:56:11 -0600

    added generate API

---
+++ apps/web/app/api/generate/[preset]/route.ts
@@ -0,0 +1,49 @@
+import { NextRequest, NextResponse } from "next/server";
+import { getAllPresets, getPreset, getSeedNumber, paramsToString } from "core";
+
+export const runtime = "edge";
+
+export type HTTPError = Error & { status?: number };
+
+type GenerateParams = {
+  preset: string
+}
+
+const resJSON = (data: any, status = 200, headers = {}) =>
+  new Response(JSON.stringify(data), {
+    status,
+    headers,
+  });
+
+export async function GET(req: NextRequest, { params }: { params: GenerateParams} ) {
+   try {
+    const seedNum = getSeedNumber();
+    const preset = getPreset(params.preset);
+
+    if (preset == undefined) {
+      const validPresets = getAllPresets()
+        .map(p => p.tags)
+        .reduce((acc, cur) => {
+        return acc.concat(cur)
+      }, []);
+      const msg = `Invalid preset. Valid presets are: ${validPresets
+        .slice(0, -1)
+        .join(", ")} or ${validPresets.slice(-1)}`;
+      const err = new Error(msg) as HTTPError;
+      err.status = 422;
+      throw err;
+    }
+    
+    const defaultOptions = {
+      DisableFanfare: 0,
+    };
+    const hash = paramsToString(seedNum, preset.settings, defaultOptions);
+    const url = new URL(`seed/${hash}`, req.nextUrl.origin);
+    return NextResponse.json({ seed: url })
+   } catch (err: unknown) {
+    console.error(err);
+    const error = err as HTTPError;
+    const status = error.status || 500;
+    return resJSON({ error: error.message }, status);
+   }
+}
