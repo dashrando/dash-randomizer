811065ed1badd1a4643498cfb3908a27556577af
kupppo <kupppo@protonmail.com>
2024-01-30 15:31:52 +0000

    fix menu for new items

---
+++ apps/web/app/events/chozo-showcase/page.tsx
@@ -0,0 +1,14 @@
+import Link from 'next/link'
+import { Wrapper } from '@/app/components/wrapper'
+import styles from './info.module.css'
+
+
+export default function TournamentPage() {
+  return (
+    <Wrapper>
+      <div style={{ maxWidth: '620px', margin: '0 auto' }}>
+        chozo showcase
+      </div>
+    </Wrapper>
+  )
+}
