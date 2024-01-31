56997d397e9f5cdf0c3d2a61cf8aa28e5fd2cadf
kupppo <kupppo@protonmail.com>
2024-01-31 11:13:29 +0000

    make better countdown animation

--- apps/web/app/events/vs-world/countdown.tsx
+++ apps/web/app/events/vs-world/countdown.tsx
@@ -4,30 +4,58 @@ import { useState, useEffect, useRef } from 'react'
 import styles from './countdown.module.css'
 import { CSSTransition, TransitionGroup } from 'react-transition-group';
 
-const Number = ({ value }: { value: string }) => {
+function calculatePrevValue(input: string, maxValue: number) {
+  const value = parseInt(input)
+  const prev = value + 1
+  if (prev >= maxValue) {
+    return '0'
+  }
+  return prev.toString()
+}
+
+const Number = ({ maxValue, value }: { maxValue: number, value: string }) => {
   const nodeRef = useRef(null)
+  const prevNodeRef = useRef(null)
+  const prevValue = calculatePrevValue(value, maxValue)
+
   return (
-    <TransitionGroup className="wah">
-      <CSSTransition
-        nodeRef={nodeRef}
-        key={value}
-        classNames={{
-          enter: styles['animateNumber-enter'],
-          enterActive: styles['animateNumber-enter'],
-          exit: styles['animateNumber-exit'],
-          exitActive: styles['animateNumber-exit'],
-        }}
-        timeout={400}
-      >
-        <div
-          ref={nodeRef}
-          className={styles.number}
-          suppressHydrationWarning
+    <div className={styles.numberWrapper}>
+      <TransitionGroup>
+        <CSSTransition
+          key={prevValue}
+          nodeRef={prevNodeRef}
+          classNames={{
+            exit: styles['animateNumber-exit'],
+          }}
+          timeout={300}
         >
-          {value}
-        </div>
-      </CSSTransition>
-    </TransitionGroup>
+          <span
+            className={`${styles.number} ${styles.numberPrev}`}
+            ref={prevNodeRef}
+            suppressHydrationWarning
+          >
+            {prevValue}
+          </span>
+        </CSSTransition>
+        <CSSTransition
+          key={value}
+          nodeRef={nodeRef}
+          classNames={{
+            enter: styles['animateNumber-enter'],
+          }}
+          timeout={300}
+        >
+          <span
+            className={styles.number}
+            ref={nodeRef}
+            suppressHydrationWarning
+          >
+            {value}
+          </span>
+        </CSSTransition>
+      </TransitionGroup>
+      <span className={styles.placeholder} suppressHydrationWarning>{value}</span>
+    </div>
   )
 }
 
@@ -52,29 +80,29 @@ export default function Countdown({ launchTime } : { launchTime: Date }) {
     <div className={styles.wrapper}>
       <div className={styles.countdown_unit}>
         <div className={styles.numbers}>
-          <Number value={days[0]} />
-          <Number value={days[1]} />
+          <Number maxValue={10} value={days[0]} />
+          <Number maxValue={10} value={days[1]} />
         </div>
         <div className={styles.label}>Days</div>
       </div>
       <div className={styles.countdown_unit}>
         <div className={styles.numbers}>
-          <Number value={hours[0]} />
-          <Number value={hours[1]} />
+          <Number maxValue={3} value={hours[0]} />
+          <Number maxValue={10} value={hours[1]} />
         </div>
         <div className={styles.label}>Hours</div>
       </div>
       <div className={styles.countdown_unit}>
         <div className={styles.numbers}>
-          <Number value={minutes[0]} />
-          <Number value={minutes[1]} />
+          <Number maxValue={6} value={minutes[0]} />
+          <Number maxValue={10} value={minutes[1]} />
         </div>
         <div className={styles.label}>Minutes</div>
       </div>
       <div className={styles.countdown_unit}>
         <div className={styles.numbers}>
-          <Number value={seconds[0]} />
-          <Number value={seconds[1]} />
+          <Number maxValue={6} value={seconds[0]} />
+          <Number maxValue={10} value={seconds[1]} />
         </div>
         <div className={styles.label}>Seconds</div>
       </div>
