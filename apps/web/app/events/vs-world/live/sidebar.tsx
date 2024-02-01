a70cba7eede0390dd6c16eef3fabb70daf4ed28f
kupppo <kupppo@protonmail.com>
2024-02-01 15:13:55 +0000

    move sidebar to its own client component

---
+++ apps/web/app/events/vs-world/live/sidebar.tsx
@@ -0,0 +1,53 @@
+'use client'
+
+import styles from '../page.module.css'
+import { MATCHES } from '../data'
+import { PropsWithChildren } from 'react'
+import Time from '../time'
+import { cn } from '@/lib/utils'
+import Badge from '@/app/components/badge'
+import { TwitchChat } from './twitch'
+
+
+const Runner = ({ children }: PropsWithChildren) => <span style={{ color: 'var(--color-highlight)' }}>{children}</span>
+
+type RaceProps = {
+  time: Date
+  runners: string[]
+  status: 'upcoming' | 'live' | 'past'
+}
+
+const Race = ({ runners, status, time }: RaceProps) => (
+  <li className={cn(styles.race, styles[`race-${status}`])}>
+    <div className={styles.raceContent}>
+      <Runner>{runners[0]}</Runner> & <Runner>{runners[1]}</Runner>
+      <span style={{ display: 'inline-block', margin: '0 var(--spacer-2x)' }}>vs</span>
+      <Runner>{runners[2]}</Runner> & <Runner>{runners[3]}</Runner>
+      <br />
+      <div className={cn(styles.raceTime, status === 'live' && styles.raceTimeLive)}>
+        <span className={styles.scheduledTime}>
+          <Time time={time} unit="timeMatch" /> on <a href="https://twitch.tv/speedgaming" target="_blank" rel="noreferrer noopener">SpeedGaming</a>
+        </span>
+        <div className={styles.liveBadge}>
+          <Badge variant="early" size="small">LIVE</Badge>
+        </div>
+      </div>
+    </div>
+  </li>
+)
+
+export default function LiveSidebar() {
+  return (
+    <aside className={styles.sidebar}>
+      <div className={styles.schedule}>
+        <h1 className={styles.title}>DASH Team vs The World</h1>
+        <ul className={styles.races}>
+          <Race status="past" runners={['kupppo', 'MassHesteria', 'AceZer0', 'ProfessorSchool']} time={MATCHES[0]} />
+          <Race status="live" runners={['PapaSchmo', 'derp', 'bressingham', 'mm2nescartridge']} time={MATCHES[1]} />
+          <Race status="upcoming" runners={['Kipp', 'cassidymoen', 'Zeb316', 'd_webb']} time={MATCHES[2]} />
+        </ul>
+      </div>
+      <TwitchChat />
+    </aside>
+  )
+}
