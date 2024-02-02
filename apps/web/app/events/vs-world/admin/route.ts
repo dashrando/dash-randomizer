bda3e8985a6d36010da34320c15de43f3ddb36cd
kupppo <kupppo@protonmail.com>
2024-02-02 17:09:35 +0000

    get origin + redirect out of try block

--- apps/web/app/events/vs-world/admin/route.ts
+++ apps/web/app/events/vs-world/admin/route.ts
@@ -2,11 +2,11 @@ import { cookies } from 'next/headers'
 import { NextRequest, NextResponse } from 'next/server'
 
 export async function GET(req: NextRequest) {
+  const origin = new URL(req.nextUrl.origin)
+  const redirect = `${origin}/events/vs-world`
   try {
     const cookieStore = cookies()
     const auth = req.nextUrl.searchParams.get('auth')
-    const origin = new URL(req.nextUrl.origin)
-    const redirect = `${origin}/events/vs-world`
     if (auth) {
       cookieStore.set('dash-auth', auth)
     }
