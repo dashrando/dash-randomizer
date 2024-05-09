df5f1ec730f8052f0b4375069dd214d97b529735
kupppo <kupppo@protonmail.com>
2024-05-09 18:21:22 +0100

    initial spoiler

--- apps/web/app/seed/race/[key]/spoiler/page.tsx
+++ apps/web/app/seed/race/[key]/spoiler/page.tsx
@@ -1,9 +1,25 @@
 import { kv } from '@vercel/kv'
+import styles from '../../../[seed]/seed.module.css'
+import { prefetchSignature, stringToParams } from 'core'
 
 type RaceSeedData = {
   hash: string
   key: string
-  spoiler: object
+  spoiler: Spoiler
+}
+
+type Spoiler = {
+  'Area Transitions': {
+    [transition: string]: string
+  }
+  Bosses: {
+    [location: string]: string
+  }
+  Items: {
+    [area: string]: {
+      [location: string]: string
+    }
+  }
 }
 
 export default async function RaceSeedSpoilerPage({ params }: { params: { key: string } }) {
@@ -14,11 +30,83 @@ export default async function RaceSeedSpoilerPage({ params }: { params: { key: s
   }
 
   const { hash, spoiler } = data as RaceSeedData
+  const seedParams = stringToParams(hash)
+  const seedNum = seedParams.seed
+  const sig = prefetchSignature(seedNum)
+  const bosses = spoiler['Bosses']
+  const areas = spoiler['Area Transitions']
+  const items = spoiler['Items']
+
   return (
-    <>
-      <div>Race Seed Spoiler</div>
-      <p>{JSON.stringify(spoiler, null, 2)}</p>
-      <a href={`/seed/race/${key}/spoiler/download`}>Download Spoiler</a>
-    </>
+    <main className={styles.container}>
+      <h1 className={styles.logo}>DASH</h1>
+      <div className={styles.signature}>{sig || <>&nbsp;</>}</div>
+      <a href={`/seed/race/${key}/spoiler/download`} style={{ color: '#ffffff', display: 'block', marginTop: '2em' }}>Download Spoiler</a>
+      <div className={styles.spoiler_data}>
+        <h3>Bosses</h3>
+        <table>
+          <thead>
+            <tr>
+              <th>Location</th>
+              <th>Boss</th>
+            </tr>
+          </thead>
+          <tbody>
+            {Object.keys(bosses).map((location) => {
+              return (
+                <tr key={location}>
+                  <td>{location}</td>
+                  <td>{bosses[location]}</td>
+                </tr>
+              )
+            })}
+          </tbody>
+        </table>
+
+        <h3>Transitions</h3>
+        <table>
+          <thead>
+            <tr>
+              <th>From</th>
+              <th>To</th>
+            </tr>
+          </thead>
+          <tbody>
+            {Object.keys(areas).map((transition) => {
+              return (
+                <tr key={transition}>
+                  <td>{transition}</td>
+                  <td>{areas[transition]}</td>
+                </tr>
+              )
+            })}
+          </tbody>
+        </table>
+
+        <h3>Items</h3>
+        {Object.keys(items).map((area) => (
+          <div key={area}>
+            <h4>{area}</h4>
+            <table>
+              <thead>
+                <tr>
+                  <th>Location</th>
+                  <th>Item</th>
+                </tr>
+              </thead>
+              <tbody>
+                {Object.keys(items[area]).map((item) => {
+                  return (
+                    <tr key={item}>
+                      <td>{item}</td>
+                      <td>{items[area][item]}</td>
+                    </tr>
+                  )})}
+                </tbody>
+            </table>
+          </div>
+        ))}
+      </div>
+    </main>
   )
 }
