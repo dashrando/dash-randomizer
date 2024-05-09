535282b7f88d47ccde2bf2c4008a206656507ecc
MassHesteria <brad.hester@gmail.com>
2024-05-08 19:53:08 -0500

    fixed download route

--- apps/web/app/seed/race/[key]/spoiler/download/route.ts
+++ apps/web/app/seed/race/[key]/spoiler/download/route.ts
@@ -1,9 +1,8 @@
-export async function GET({ params }: { params: { key: string } }) {
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(req: NextRequest, { params }: { params: { key: string } }) {
   // TODO: Check for valid password via cookie
   // TODO: Download a plaintext vesion of the spoiler log
-  return {
-    status: 200,
-    headers: {},
-    body: JSON.stringify({ key: params.key }),
-  }
+
+  return new NextResponse(JSON.stringify({ key: params.key }, null, 2));
 }
