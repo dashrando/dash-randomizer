29016ecd7d63492650e9b3a4475503110fe69b73
kupppo <kupppo@protonmail.com>
2024-05-09 05:29:17 +0100

    use custom alphabet for nanoid + nextresponse redirect

--- apps/web/app/generate/[preset]/route.ts
+++ apps/web/app/generate/[preset]/route.ts
@@ -1,21 +1,23 @@
-import { NextRequest } from "next/server";
-import { getAllPresets, getPreset, getSeedNumber, paramsToString } from "core";
-import { nanoid } from 'nanoid'
+import { NextRequest, NextResponse } from "next/server"
+import { getAllPresets, getPreset, getSeedNumber, paramsToString } from "core"
+import { customAlphabet } from 'nanoid'
 import { kv } from '@vercel/kv'
-import { getSpoiler } from "@/lib/spoiler";
+import { getSpoiler } from "@/lib/spoiler"
 
-export const runtime = "edge";
+export const runtime = "edge"
 
-export type HTTPError = Error & { status?: number };
+export type HTTPError = Error & { status?: number }
 
 type GenerateParams = {
   preset: string
 }
 
 export function GET(req: NextRequest, { params }: { params: GenerateParams} ) {
+   const nanoid = customAlphabet('0123456789abcdefghijklmnopqrstuvwxyz', 12)
+
    try {
-    const seedNum = getSeedNumber();
-    const preset = getPreset(params.preset);
+    const seedNum = getSeedNumber()
+    const preset = getPreset(params.preset)
     const searchParams = req.nextUrl.searchParams
     const spoiler = searchParams.get('spoiler') || 0
     const race = searchParams.get('race') || 0
@@ -28,40 +30,38 @@ export function GET(req: NextRequest, { params }: { params: GenerateParams} ) {
       }, []);
       const msg = `Invalid preset. Valid presets are: ${validPresets
         .slice(0, -1)
-        .join(", ")} or ${validPresets.slice(-1)}`;
-      const err = new Error(msg) as HTTPError;
-      err.status = 422;
-      throw err;
+        .join(", ")} or ${validPresets.slice(-1)}`
+      const err = new Error(msg) as HTTPError
+      err.status = 422
+      throw err
     }
 
     if (spoiler && !race) {
-      const err = new Error('Spoilers are currently only valid for race seeds') as HTTPError;
-      err.status = 422;
-      throw err;
+      const err = new Error('Spoilers are currently only valid for race seeds') as HTTPError
+      err.status = 422
+      throw err
     }
 
     if (race) {
       const hash = paramsToString(seedNum, preset.settings, preset.options)
-      console.log('hash:',hash)
       const raceObj = {
         key: nanoid(10),
         hash,
         spoiler: spoiler ? getSpoiler(hash) : undefined
       }
+      console.log(raceObj)
       kv.hset(`race-${raceObj.key}`, raceObj)
-      const url = new URL(`seed/race/${raceObj.key}`, req.nextUrl.origin);
-      return new Response(url.toString(),
-        { status: 307, headers: {Location: url.toString()} });
+      const url = new URL(`seed/race/${raceObj.key}`, req.nextUrl.origin)
+      return NextResponse.redirect(url.toString(), { status: 307 })
     }
     
-    const hash = paramsToString(seedNum, preset.settings, preset.options);
-    const url = new URL(`seed/${hash}`, req.nextUrl.origin);
-    return new Response(url.toString(),
-      { status: 307, headers: {Location: url.toString()} });
+    const hash = paramsToString(seedNum, preset.settings, preset.options)
+    const url = new URL(`seed/${hash}`, req.nextUrl.origin)
+    return NextResponse.redirect(url.toString(), { status: 307 })
    } catch (err: unknown) {
-    console.error(err);
-    const error = err as HTTPError;
-    const status = error.status || 500;
-    return new Response(error.message, { status } );
+    console.error(err)
+    const error = err as HTTPError
+    const status = error.status || 500
+    return new Response(error.message, { status } )
    }
 }
