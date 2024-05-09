b7a9a254f95c74e26e915f63dfe54981b993be50
kupppo <kupppo@protonmail.com>
2024-05-09 06:36:59 +0100

    spoiler code uses single redis command

--- apps/web/app/seed/race/[key]/page.tsx
+++ apps/web/app/seed/race/[key]/page.tsx
@@ -1,18 +1,32 @@
 import { kv } from '@vercel/kv'
 import { stringToParams } from 'core'
 
+type RaceSeedData = {
+  hash: string
+  key: string
+  spoiler: object
+}
+
 export default async function RaceSeedPage({ params }: { params: { key: string } }) {
   const { key } = params
-  const hash = await kv.hget<string>(`race-${key}`, 'hash')
-  const spoiler = await kv.hget<string>(`race-${key}`, 'spoiler')
+  const data = await kv.hgetall(`race-${key}`)
+  const { hash, spoiler } = data as RaceSeedData
   const seedParams = hash ? stringToParams(hash) : null
-  return (<>
-  <div>Race Seed</div>
-  <p>
-  {spoiler !== null &&
-  <a href={`/seed/race/${key}/spoiler`}>Spoiler</a>
-  }
-  </p>
-  <div>{JSON.stringify(seedParams, null, 2)}</div>
-  </>)
+  return (
+    <>
+      <div>Race Seed</div>
+        {spoiler && (
+          <p>
+            <a href={`/seed/race/${key}/spoiler`}>View Spoiler</a>
+            <br />
+            <a href={`/seed/race/${key}/spoiler/download`}>Download Spoiler</a>
+          </p>
+        )}
+        <div>
+          <pre>
+            <code>{JSON.stringify(seedParams, null, 2)}</code>
+          </pre>
+        </div>
+    </>
+  )
 }
--- apps/web/app/seed/race/[key]/spoiler/page.tsx
+++ apps/web/app/seed/race/[key]/spoiler/page.tsx
@@ -1,13 +1,24 @@
 import { kv } from '@vercel/kv'
 
+type RaceSeedData = {
+  hash: string
+  key: string
+  spoiler: object
+}
+
 export default async function RaceSeedSpoilerPage({ params }: { params: { key: string } }) {
   const { key } = params
-  const spoiler = await kv.hget<string>(`race-${key}`, 'spoiler')
+  const data = await kv.hgetall(`race-${key}`)
+  if (!data) {
+    return <div>Seed not found</div>
+  }
+
+  const { hash, spoiler } = data as RaceSeedData
   return (
     <>
-  <div>Race Seed Spoiler</div>
-  <p>{JSON.stringify(spoiler, null, 2)}</p>
-  <a href={`/seed/race/${key}/spoiler/download`}>Spoiler</a>
-  </>
+      <div>Race Seed Spoiler</div>
+      <p>{JSON.stringify(spoiler, null, 2)}</p>
+      <a href={`/seed/race/${key}/spoiler/download`}>Download Spoiler</a>
+    </>
   )
 }
