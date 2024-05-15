eb987a0d02f075009a5f987f1d95dc932a21401c
kupppo <kupppo@protonmail.com>
2024-05-15 15:30:26 +0100

    refactor race URL calls

--- apps/web/app/race/[key]/spoiler/page.tsx
+++ apps/web/app/race/[key]/spoiler/page.tsx
@@ -124,7 +124,7 @@ export default async function RaceSeedSpoilerPage({ params }: { params: { key: s
       </div>
       <footer className={styles.footer}>
         <p>
-          <Link href={`/seed/race/${key}`}>Return to Seed Page</Link>
+          <Link href={`/race/${key}`}>Return to Seed Page</Link>
         </p>
       </footer>
     </main>
--- apps/web/app/seed/[seed]/seed.tsx
+++ apps/web/app/seed/[seed]/seed.tsx
@@ -132,7 +132,7 @@ export default function Seed({
             </Button>
             {spoiler && (
               <div className={styles.spoiler_link}>
-                <Link href={`/seed/race/${slug}/spoiler`}>
+                <Link href={`/race/${slug}/spoiler`}>
                   View Spoiler Log
                 </Link>
               </div>
