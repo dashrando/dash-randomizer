0471302c6095c250cd20437dd8adaafb77647497
kupppo <kupppo@protonmail.com>
2024-05-19 22:17:18 +0100

    spring invitational styles

--- apps/web/app/events/spring-invitational/page.tsx
+++ apps/web/app/events/spring-invitational/page.tsx
@@ -1,5 +1,6 @@
 import { Wrapper } from '@/app/components/wrapper'
 import styles from '../vs-world/event.module.css'
+import { ButtonLink } from '@/app/components/button'
 
 export const metadata = {
   title: 'DASH Spring Invitational',
@@ -21,7 +22,10 @@ export default function TournamentPage() {
               <span>May 23 - 27</span>
             </h3>
           </div>
-          <div style={{ margin: 'var(--spacer-32x) 0 var(--spacer-12x)'}}>
+          <div style={{ margin: 'var(--spacer-12x) 0 var(--spacer-12x)', textAlign: 'center' }}>
+            <ButtonLink variant="hero" href="/spring-invitational-form" style={{ margin: '0 auto' }} target="_blank">Register your interest</ButtonLink>
+          </div>
+          <div style={{ margin: 'var(--spacer-24x) 0 var(--spacer-12x)'}}>
             <h3 className={styles.settingsTitle}>Settings</h3>
             <ul className={styles.settings}>
               <li>
@@ -47,6 +51,30 @@ export default function TournamentPage() {
               <li>
                 <span className={styles.label}>Environment Updates</span>
                 Standard
+              </li>
+              <li>
+                <span className={styles.label}>Charge Beam</span>
+                Starter+
+              </li>
+              <li>
+                <span className={styles.label}>Gravity Heat Reduction</span>
+                Off
+              </li>
+              <li>
+                <span className={styles.label}>Double Jump</span>
+                Off
+              </li>
+              <li>
+                <span className={styles.label}>Heat Shield</span>
+                Off
+              </li>
+              <li>
+                <span className={styles.label}>Pressure Valve</span>
+                Off
+              </li>
+              <li>
+                <span className={styles.label}>Item Fanfare</span>
+                On
               </li>
             </ul>
           </div>
