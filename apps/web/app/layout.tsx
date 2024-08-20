ffea2fc669f6bb95b82e01c35a5e52e22fbc9744
MassHesteria <brad.hester@gmail.com>
2024-08-20 07:00:21 -0500

    self hosting inter font

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,7 +1,5 @@
+import '../public/fonts/inter/inter.css'
 import './globals.css'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'DASH Randomizer',
@@ -15,7 +13,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body>{children}</body>
     </html>
   )
 }
