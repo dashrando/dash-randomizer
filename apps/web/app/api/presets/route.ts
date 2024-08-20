68636d574dd152eb9b7ac3430c5aaf10c5bcb924
MassHesteria <brad.hester@gmail.com>
2024-08-20 07:08:33 -0500

    static presets route for now

--- apps/web/app/api/presets/route.ts
+++ apps/web/app/api/presets/route.ts
@@ -1,7 +1,7 @@
 import { getAllPresets } from "core";
 import { NextResponse } from "next/server";
 
-export const runtime = "edge";
+//export const runtime = "edge";
 
 export async function GET() {
   const presets = getAllPresets().map((p) => {
